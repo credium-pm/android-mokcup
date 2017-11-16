7e0e1cde699805ae7144d47e13b10e61fca398c5
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-11-16 17:41:30 +0200

    Add settings activity.

--- app/src/main/java/com/credium/MainActivity.kt
+++ app/src/main/java/com/credium/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.credium
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import com.credium.settings.SettingsActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,18 +25,22 @@ class MainActivity : AppCompatActivity() {
         drawerToggle.syncState()
 
         navigationView.setNavigationItemSelectedListener { item ->
-            val (fragment, title) = when (item.itemId) {
-            // TODO: handle particular item click
-                R.id.nav_loans -> LoansFragment() to getString(R.string.loans)
-                else -> null to null
-            }
-            fragment?.let {
+            if (item.itemId == R.id.nav_settings)
+                startActivity(Intent(this, SettingsActivity::class.java))
+            else {
+                val (fragment, titleStringResource) = when (item.itemId) {
+                    R.id.nav_dashboard -> Fragment() to R.string.dashboard
+                    R.id.nav_portfolio -> Fragment() to R.string.portfolio
+                    R.id.nav_market -> Fragment() to R.string.market
+                    R.id.nav_loans -> LoansFragment() to R.string.loans
+                    else -> throw RuntimeException("No handler for item id: ${item.itemId}")
+                }
                 supportFragmentManager.beginTransaction()
-                        .replace(R.id.containerFrame, it)
+                        .replace(R.id.containerFrame, fragment)
                         .commit()
-                this@MainActivity.title = title!!
+                this.title = getString(titleStringResource)
+                drawerLayout.closeDrawer(GravityCompat.START)
             }
-            drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
     }
---
+++ app/src/main/java/com/credium/settings/SettingsActivity.kt
@@ -0,0 +1,6 @@
+package com.credium.settings
+
+import android.support.v7.app.AppCompatActivity
+
+
+class SettingsActivity : AppCompatActivity()
