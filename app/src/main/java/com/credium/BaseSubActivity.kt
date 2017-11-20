206185b8eac3bb42c36a50b1e28df835f16280dc
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-11-20 19:43:04 +0200

    Set gradient toolbar.

--- app/src/main/java/com/credium/BaseSubActivity.kt
+++ app/src/main/java/com/credium/BaseSubActivity.kt
@@ -3,6 +3,7 @@ package com.credium
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import com.credium.util.setActionBarDrawable
 import com.credium.util.showHomeUpNavigation
 
 
@@ -10,6 +11,7 @@ open class BaseSubActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setActionBarDrawable(R.drawable.bg_blue_gradient)
         showHomeUpNavigation()
     }
 
