cd711bb13b8f863b7bdf117eb89a3a7296f4de37
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-11-21 21:46:24 +0200

    Empty db by default.

--- app/src/main/java/com/credium/data/LoansProvider.kt
+++ app/src/main/java/com/credium/data/LoansProvider.kt
@@ -3,11 +3,7 @@ package com.credium.data
 
 object LoansProvider {
 
-    private val loans = mutableListOf<Loan>(
-            LockedLoan(id = 1, currency = DollarUsaCurrency, amount = 1000, amountPerMonth = 50, months = 12).apply { isLocked = false },
-            LockedLoan(id = 2, currency = DollarUsaCurrency, amount = 1500, amountPerMonth = 150, months = 10),
-            LockedLoan(id = 3, currency = DollarUsaCurrency, amount = 2100, amountPerMonth = 300, months = 7)
-    )
+    private val loans = mutableListOf<Loan>()
 
     fun addLoans(loans: List<Loan>) {
         this.loans.addAll(loans)
