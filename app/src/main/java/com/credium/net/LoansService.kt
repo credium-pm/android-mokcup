0866e3c9909fb70d690afa4b529a8d60ab4d8fde
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-11-24 14:21:26 +0200

    Add more loans for notes screen.

--- app/src/main/java/com/credium/net/LoansService.kt
+++ app/src/main/java/com/credium/net/LoansService.kt
@@ -28,78 +28,63 @@ object LoansService {
     )
 
     fun fetchPortfolioNotes() = listOf(
-            Note(loan = UnlockedLoan(id = 111, currency = DollarUsaCurrency, amount = 1000, amountPerMonth = 50, months = 12),
-                    riskPercentage = 89,
-                    apr = 13,
-                    riskScore = RiskScore.INCREASE),
-            Note(loan = UnlockedLoan(id = 222, currency = DollarUsaCurrency, amount = 180, amountPerMonth = 30, months = 6),
-                    riskPercentage = 67,
-                    apr = 19,
-                    riskScore = RiskScore.DECREASE),
-            Note(loan = UnlockedLoan(id = 333, currency = DollarUsaCurrency, amount = 75, amountPerMonth = 15, months = 5),
-                    riskPercentage = 73,
-                    apr = 10,
-                    riskScore = RiskScore.NORMAL),
-            Note(loan = UnlockedLoan(id = 444, currency = DollarUsaCurrency, amount = 1000, amountPerMonth = 50, months = 12),
-                    riskPercentage = 89,
-                    apr = 13,
-                    riskScore = RiskScore.INCREASE),
-            Note(loan = UnlockedLoan(id = 555, currency = DollarUsaCurrency, amount = 180, amountPerMonth = 30, months = 6),
-                    riskPercentage = 67,
-                    apr = 19,
-                    riskScore = RiskScore.DECREASE),
-            Note(loan = UnlockedLoan(id = 666, currency = DollarUsaCurrency, amount = 75, amountPerMonth = 15, months = 5),
-                    riskPercentage = 73,
-                    apr = 10,
-                    riskScore = RiskScore.NORMAL)
+            Note(loan = UnlockedLoan(id = 3112, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76), riskPercentage = 89, apr = 13, riskScore = RiskScore.NORMAL),
+            Note(loan = UnlockedLoan(id = 3113, currency = DollarUsaCurrency, amount = 12_250, amountPerMonth = 2500, months = 6, sellFor = 1920, discount = 40, riskScore = 77), riskPercentage = 78, apr = 13, riskScore = RiskScore.DECREASE),
+            Note(loan = UnlockedLoan(id = 3114, currency = DollarUsaCurrency, amount = 15_000, amountPerMonth = 2500, months = 6, sellFor = 3000, discount = 25, riskScore = 72), riskPercentage = 89, apr = 13, riskScore = RiskScore.INCREASE),
+            Note(loan = UnlockedLoan(id = 3115, currency = DollarUsaCurrency, amount = 18_000, amountPerMonth = 2250, months = 8, sellFor = 3840, discount = 36, riskScore = 89), riskPercentage = 89, apr = 13, riskScore = RiskScore.NORMAL),
+            Note(loan = UnlockedLoan(id = 3116, currency = DollarUsaCurrency, amount = 17_550, amountPerMonth = 1350, months = 13, sellFor = 3775, discount = 50, riskScore = 76), riskPercentage = 81, apr = 13, riskScore = RiskScore.DECREASE),
+            Note(loan = UnlockedLoan(id = 3117, currency = DollarUsaCurrency, amount = 76_800, amountPerMonth = 3200, months = 24, sellFor = 8000, discount = 20, riskScore = 71), riskPercentage = 89, apr = 13, riskScore = RiskScore.DECREASE),
+            Note(loan = UnlockedLoan(id = 3118, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76), riskPercentage = 77, apr = 13, riskScore = RiskScore.INCREASE),
+            Note(loan = UnlockedLoan(id = 3112, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76), riskPercentage = 89, apr = 13, riskScore = RiskScore.NORMAL),
+            Note(loan = UnlockedLoan(id = 3113, currency = DollarUsaCurrency, amount = 12_250, amountPerMonth = 2500, months = 6, sellFor = 1920, discount = 40, riskScore = 77), riskPercentage = 78, apr = 13, riskScore = RiskScore.DECREASE),
+            Note(loan = UnlockedLoan(id = 3114, currency = DollarUsaCurrency, amount = 15_000, amountPerMonth = 2500, months = 6, sellFor = 3000, discount = 25, riskScore = 72), riskPercentage = 89, apr = 13, riskScore = RiskScore.INCREASE),
+            Note(loan = UnlockedLoan(id = 3115, currency = DollarUsaCurrency, amount = 18_000, amountPerMonth = 2250, months = 8, sellFor = 3840, discount = 36, riskScore = 89), riskPercentage = 89, apr = 13, riskScore = RiskScore.NORMAL),
+            Note(loan = UnlockedLoan(id = 3116, currency = DollarUsaCurrency, amount = 17_550, amountPerMonth = 1350, months = 13, sellFor = 3775, discount = 50, riskScore = 76), riskPercentage = 81, apr = 13, riskScore = RiskScore.DECREASE),
+            Note(loan = UnlockedLoan(id = 3117, currency = DollarUsaCurrency, amount = 76_800, amountPerMonth = 3200, months = 24, sellFor = 8000, discount = 20, riskScore = 71), riskPercentage = 89, apr = 13, riskScore = RiskScore.DECREASE),
+            Note(loan = UnlockedLoan(id = 3118, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76), riskPercentage = 77, apr = 13, riskScore = RiskScore.INCREASE)
     )
 
-    fun fetchMarketNotes(): List<UnlockedLoan> = listOf(
-            UnlockedLoan(
-                    id = 111,
-                    currency = DollarUsaCurrency,
-                    amount = 1000,
-                    amountPerMonth = 50,
-                    months = 20,
-                    sellFor = 450,
-                    discount = 25,
-                    riskScore = 89
-            ),
-            UnlockedLoan(
-                    id = 222,
-                    currency = DollarUsaCurrency,
-                    amount = 600,
-                    amountPerMonth = 30,
-                    months = 20,
-                    sellFor = 450,
-                    discount = 25,
-                    riskScore = 76
-            )
+    fun fetchMarketNotes(): List<UnlockedLoan> = listOf(UnlockedLoan(id = 111, currency = DollarUsaCurrency, amount = 20_250, amountPerMonth = 2250, months = 8, sellFor = 4760, discount = 32, riskScore = 89),
+            UnlockedLoan(id = 112, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76),
+            UnlockedLoan(id = 113, currency = DollarUsaCurrency, amount = 12_250, amountPerMonth = 2500, months = 6, sellFor = 1920, discount = 40, riskScore = 77),
+            UnlockedLoan(id = 114, currency = DollarUsaCurrency, amount = 15_000, amountPerMonth = 2500, months = 6, sellFor = 3000, discount = 25, riskScore = 72),
+            UnlockedLoan(id = 115, currency = DollarUsaCurrency, amount = 18_000, amountPerMonth = 2250, months = 8, sellFor = 3840, discount = 36, riskScore = 89),
+            UnlockedLoan(id = 116, currency = DollarUsaCurrency, amount = 17_550, amountPerMonth = 1350, months = 13, sellFor = 3775, discount = 50, riskScore = 76),
+            UnlockedLoan(id = 117, currency = DollarUsaCurrency, amount = 76_800, amountPerMonth = 3200, months = 24, sellFor = 8000, discount = 20, riskScore = 71),
+            UnlockedLoan(id = 118, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76),
+            UnlockedLoan(id = 119, currency = DollarUsaCurrency, amount = 12_250, amountPerMonth = 2500, months = 6, sellFor = 1920, discount = 40, riskScore = 77),
+            UnlockedLoan(id = 121, currency = DollarUsaCurrency, amount = 15_000, amountPerMonth = 2500, months = 6, sellFor = 3000, discount = 25, riskScore = 72),
+            UnlockedLoan(id = 122, currency = DollarUsaCurrency, amount = 18_000, amountPerMonth = 2250, months = 8, sellFor = 3840, discount = 36, riskScore = 89),
+            UnlockedLoan(id = 123, currency = DollarUsaCurrency, amount = 17_550, amountPerMonth = 1350, months = 13, sellFor = 3775, discount = 50, riskScore = 76),
+            UnlockedLoan(id = 124, currency = DollarUsaCurrency, amount = 76_800, amountPerMonth = 3200, months = 24, sellFor = 8000, discount = 20, riskScore = 71),
+            UnlockedLoan(id = 125, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76),
+            UnlockedLoan(id = 126, currency = DollarUsaCurrency, amount = 12_250, amountPerMonth = 2500, months = 6, sellFor = 1920, discount = 40, riskScore = 77),
+            UnlockedLoan(id = 127, currency = DollarUsaCurrency, amount = 15_000, amountPerMonth = 2500, months = 6, sellFor = 3000, discount = 25, riskScore = 72),
+            UnlockedLoan(id = 128, currency = DollarUsaCurrency, amount = 18_000, amountPerMonth = 2250, months = 8, sellFor = 3840, discount = 36, riskScore = 89),
+            UnlockedLoan(id = 130, currency = DollarUsaCurrency, amount = 17_550, amountPerMonth = 1350, months = 13, sellFor = 3775, discount = 50, riskScore = 76),
+            UnlockedLoan(id = 131, currency = DollarUsaCurrency, amount = 76_800, amountPerMonth = 3200, months = 24, sellFor = 8000, discount = 20, riskScore = 71),
+            UnlockedLoan(id = 132, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76),
+            UnlockedLoan(id = 133, currency = DollarUsaCurrency, amount = 12_250, amountPerMonth = 2500, months = 6, sellFor = 1920, discount = 40, riskScore = 77),
+            UnlockedLoan(id = 134, currency = DollarUsaCurrency, amount = 15_000, amountPerMonth = 2500, months = 6, sellFor = 3000, discount = 25, riskScore = 72),
+            UnlockedLoan(id = 135, currency = DollarUsaCurrency, amount = 18_000, amountPerMonth = 2250, months = 8, sellFor = 3840, discount = 36, riskScore = 89),
+            UnlockedLoan(id = 136, currency = DollarUsaCurrency, amount = 17_550, amountPerMonth = 1350, months = 13, sellFor = 3775, discount = 50, riskScore = 76),
+            UnlockedLoan(id = 137, currency = DollarUsaCurrency, amount = 76_800, amountPerMonth = 3200, months = 24, sellFor = 8000, discount = 20, riskScore = 71),
+            UnlockedLoan(id = 138, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76),
+            UnlockedLoan(id = 139, currency = DollarUsaCurrency, amount = 12_250, amountPerMonth = 2500, months = 6, sellFor = 1920, discount = 40, riskScore = 77),
+            UnlockedLoan(id = 140, currency = DollarUsaCurrency, amount = 15_000, amountPerMonth = 2500, months = 6, sellFor = 3000, discount = 25, riskScore = 72),
+            UnlockedLoan(id = 141, currency = DollarUsaCurrency, amount = 18_000, amountPerMonth = 2250, months = 8, sellFor = 3840, discount = 36, riskScore = 89),
+            UnlockedLoan(id = 142, currency = DollarUsaCurrency, amount = 17_550, amountPerMonth = 1350, months = 13, sellFor = 3775, discount = 50, riskScore = 76),
+            UnlockedLoan(id = 143, currency = DollarUsaCurrency, amount = 76_800, amountPerMonth = 3200, months = 24, sellFor = 8000, discount = 20, riskScore = 71)
     )
 
     fun fetchSellOrderNotes(): List<UnlockedLoan> = listOf(
-            UnlockedLoan(
-                    id = 1111,
-                    currency = DollarUsaCurrency,
-                    amount = 1000,
-                    amountPerMonth = 50,
-                    months = 20,
-                    sellFor = 450,
-                    discount = 25,
-                    riskScore = 89,
-                    daysOnMarket = 1
-            ),
-            UnlockedLoan(
-                    id = 2222,
-                    currency = DollarUsaCurrency,
-                    amount = 600,
-                    amountPerMonth = 30,
-                    months = 20,
-                    sellFor = 450,
-                    discount = 25,
-                    riskScore = 76,
-                    daysOnMarket = 1
-            )
+            UnlockedLoan(id = 2137, currency = DollarUsaCurrency, amount = 76_800, amountPerMonth = 3200, months = 24, sellFor = 8000, discount = 20, riskScore = 71, daysOnMarket = 7),
+            UnlockedLoan(id = 2138, currency = DollarUsaCurrency, amount = 36_000, amountPerMonth = 30, months = 20, sellFor = 4125, discount = 25, riskScore = 76, daysOnMarket = 2),
+            UnlockedLoan(id = 2139, currency = DollarUsaCurrency, amount = 12_250, amountPerMonth = 2500, months = 6, sellFor = 1920, discount = 40, riskScore = 77, daysOnMarket = 1),
+            UnlockedLoan(id = 2140, currency = DollarUsaCurrency, amount = 15_000, amountPerMonth = 2500, months = 6, sellFor = 3000, discount = 25, riskScore = 72, daysOnMarket = 3),
+            UnlockedLoan(id = 2141, currency = DollarUsaCurrency, amount = 18_000, amountPerMonth = 2250, months = 8, sellFor = 3840, discount = 36, riskScore = 89, daysOnMarket = 3),
+            UnlockedLoan(id = 2142, currency = DollarUsaCurrency, amount = 17_550, amountPerMonth = 1350, months = 13, sellFor = 3775, discount = 50, riskScore = 76, daysOnMarket = 10),
+            UnlockedLoan(id = 2143, currency = DollarUsaCurrency, amount = 76_800, amountPerMonth = 3200, months = 24, sellFor = 8000, discount = 20, riskScore = 71, daysOnMarket = 1)
     )
 
 }
